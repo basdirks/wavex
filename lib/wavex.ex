8c0bfb0da05d9739e6e8f4973ffa6cc3ea186eb0
Bas Dirks <bas.dirks@bettyblocks.com>
2018-04-30 21:33:04 +0200

    Adds description, adjusts module type, adjusts &read/1 return type.

--- lib/wavex.ex
+++ lib/wavex.ex
@@ -1,16 +1,24 @@
 defmodule Wavex do
-  @moduledoc false
+  @moduledoc """
+  Read PCM WAVE files.
+  """
 
   alias Wavex.{DataChunk, FormatChunk, RIFFHeader}
 
   defstruct [:riff_header, :format_chunk, :data_chunk]
 
-  @spec read(binary) :: %__MODULE__{} | {:error, binary}
+  @type t :: %__MODULE__{
+          riff_header: RIFFHeader.t(),
+          format_chunk: FormatChunk.t(),
+          data_chunk: DataChunk.t()
+        }
+
+  @spec read(binary) :: {:ok, t} | {:error, binary}
   def read(binary) when is_binary(binary) do
     with {:ok, %RIFFHeader{} = riff_header, etc} <- RIFFHeader.read(binary),
          {:ok, %FormatChunk{} = format_chunk, etc} <- FormatChunk.read(etc),
          {:ok, %DataChunk{}} = data_chunk <- DataChunk.read(etc) do
-      %Wavex{riff_header: riff_header, format_chunk: format_chunk, data_chunk: data_chunk}
+      {:ok, %Wavex{riff_header: riff_header, format_chunk: format_chunk, data_chunk: data_chunk}}
     else
       error -> error
     end
