daa8bc3fb30deea625932a7f8cf44a8416229d41
Bas Dirks <bas.dirks@bettyblocks.com>
2018-04-30 18:53:47 +0200

    First sketch.

--- lib/wavex.ex
+++ lib/wavex.ex
@@ -1,18 +1,18 @@
 defmodule Wavex do
-  @moduledoc """
-  Documentation for Wavex.
-  """
+  @moduledoc false
 
-  @doc """
-  Hello world.
+  alias Wavex.{DataChunk, FormatChunk, RIFFHeader}
 
-  ## Examples
+  defstruct [:riff_header, :format_chunk, :data_chunk]
 
-      iex> Wavex.hello
-      :world
-
-  """
-  def hello do
-    :world
+  @spec read(binary) :: %__MODULE__{} | {:error, binary}
+  def read(binary) when is_binary(binary) do
+    with {:ok, %RIFFHeader{} = riff_header, etc} <- RIFFHeader.read(binary),
+         {:ok, %FormatChunk{} = format_chunk, etc} <- FormatChunk.read(etc),
+         {:ok, %DataChunk{}} = data_chunk <- DataChunk.read(etc) do
+      %Wavex{riff_header: riff_header, format_chunk: format_chunk, data_chunk: data_chunk}
+    else
+      error -> error
+    end
   end
 end
