4227aa068fc35f384ee0a4fcba00fdde1091f5ac
Bas Dirks <bas.dirks@bettyblocks.com>
2018-05-01 08:24:35 +0200

    Enables documentation, simplifies typespec.

--- lib/wavex/utils.ex
+++ lib/wavex/utils.ex
@@ -1,11 +1,13 @@
 defmodule Wavex.Utils do
-  @moduledoc false
+  @moduledoc """
+  Reading utilities.
+  """
 
   @spec verify(binary, t, t) :: :ok | {:error, binary} when t: var
   def verify(_, a, a), do: :ok
   def verify(label, a, b), do: {:error, "expected #{label} '#{a}', got: '#{b}'"}
 
-  @spec read_id(binary, binary) :: {:ok, binary} | {:error, binary}
+  @spec read_id(binary, binary) :: {:ok | :error, binary}
   def read_id(<<id::binary-size(4), etc::binary>>, expected_id) do
     case id do
       ^expected_id -> {:ok, etc}
