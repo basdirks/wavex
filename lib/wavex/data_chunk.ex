4160949163b7f319f6f63db4a7af2b6e6c76d75a
Bas Dirks <bas.dirks@bettyblocks.com>
2018-05-01 17:31:20 +0200

    Adjusts for block align, adds doctest.

--- lib/wavex/data_chunk.ex
+++ lib/wavex/data_chunk.ex
@@ -1,6 +1,6 @@
 defmodule Wavex.DataChunk do
   @moduledoc """
-  Read a data chunk.
+  Reading a data chunk.
   """
 
   alias Wavex.Utils
@@ -11,12 +11,21 @@ defmodule Wavex.DataChunk do
 
   @doc ~S"""
   Read a data chunk.
+
+  ## Examples
+
+      iex> Wavex.DataChunk.read(<<0x0064, 0x0061, 0x0074, 0x0061,
+      ...>                        0x0002, 0x0000, 0x0000, 0x0000,
+      ...>                        0x0000, 0x0000, 0x0000, 0x0000,
+      ...>                        0x0000, 0x0000, 0x0000, 0x0000>>, 4)
+      {:ok, %Wavex.DataChunk{size: 2, data: <<0, 0, 0, 0, 0, 0, 0, 0>>}}
+
   """
-  @spec read(binary) :: {:ok, t} | {:error, binary}
-  def read(binary) when is_binary(binary) do
+  @spec read(binary, non_neg_integer) :: {:ok, t} | {:error, binary}
+  def read(binary, block_align) do
     with {:ok, etc} <- Utils.read_id(binary, "data"),
          <<size::32-little, etc::binary>> <- etc,
-         :ok <- Utils.verify("data length", String.length(etc), 8 * size) do
+         :ok <- Utils.verify("data length", String.length(etc), size * block_align) do
       {:ok, %__MODULE__{size: size, data: etc}}
     else
       etc when is_binary(etc) -> {:error, "unexpected EOF"}
