19edd9c2f98a05d57b1b1a51e19a5a6b489609ea
Bas Dirks <bas.dirks@bettyblocks.com>
2018-05-08 18:53:09 +0200

    0.4.5: Restores a doctest.

--- lib/wavex.ex
+++ lib/wavex.ex
@@ -21,7 +21,7 @@ defmodule Wavex do
 
   ## Examples
       
-      iex Wavex.read(<<
+      iex> Wavex.read(<<
       ...>   0x52, 0x49, 0x46, 0x46, #  R     I     F     F
       ...>   0x34, 0x00, 0x00, 0x00, #  52
       ...>   0x57, 0x41, 0x56, 0x45, #  W     A     V     E
@@ -32,25 +32,25 @@ defmodule Wavex do
       ...>   0x88, 0x58, 0x01, 0x00, #  88200
       ...>   0x04, 0x00, 0x10, 0x00, #  4           16
       ...>   0x64, 0x61, 0x74, 0x61, #  d     a     t     a
-      ...>   0x02, 0x00, 0x00, 0x00, #  2
+      ...>   0x08, 0x00, 0x00, 0x00, #  8
       ...>   0x00, 0x00, 0x00, 0x00, #  0
       ...>   0x00, 0x00, 0x00, 0x00  #  0
       ...> >>)
       {:ok,
-      %Wavex{
-        data_chunk: %Wavex.DataChunk{
-          data: <<0, 0, 0, 0, 0, 0, 0, 0>>,
-          size: 6
-        },
-        format_chunk: %Wavex.FormatChunk{
-          bits_per_sample: 16,
-          block_align: 4,
-          byte_rate: 88_200,
-          channels: 2,
-          sample_rate: 22_050
-        },
-        riff_header: %Wavex.RIFFHeader{size: 52}
-      }}
+       %Wavex{
+         data_chunk: %Wavex.DataChunk{
+           data: <<0, 0, 0, 0, 0, 0, 0, 0>>,
+           size: 8
+         },
+         format_chunk: %Wavex.FormatChunk{
+           bits_per_sample: 16,
+           block_align: 4,
+           byte_rate: 88_200,
+           channels: 2,
+           sample_rate: 22_050
+         },
+         riff_header: %Wavex.RIFFHeader{size: 52}
+       }}
 
       iex> Wavex.read(<<
       ...>   0x52, 0x49, 0x46, 0x46, #  R     I     F     F
@@ -67,17 +67,17 @@ defmodule Wavex do
       ...>   0x00, 0x00, 0xFE, 0xFF  #  0     0     254   255
       ...> >>)
       {:ok,
-        %Wavex{
-          data_chunk: %Wavex.DataChunk{data: <<0, 0, 254, 255>>, size: 4},
-          format_chunk: %Wavex.FormatChunk{
-            bits_per_sample: 16,
-            block_align: 2,
-            byte_rate: 22_050,
-            channels: 1,
-            sample_rate: 11_025
-          },
-          riff_header: %Wavex.RIFFHeader{size: 48}
-        }}
+       %Wavex{
+         data_chunk: %Wavex.DataChunk{data: <<0, 0, 254, 255>>, size: 4},
+         format_chunk: %Wavex.FormatChunk{
+           bits_per_sample: 16,
+           block_align: 2,
+           byte_rate: 22_050,
+           channels: 1,
+           sample_rate: 11_025
+         },
+         riff_header: %Wavex.RIFFHeader{size: 48}
+       }}
 
   """
   @spec read(binary) :: {:ok, t} | {:error, Error.t()}
